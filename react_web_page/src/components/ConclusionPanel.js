0280a70e6352323e8c044e947e4e8913f7a42c91
Aleksandr Leisle <113707601+HerrPhoton@users.noreply.github.com>
2024-09-25 11:44:26 +0700

    Заглушки

--- react_web_page/src/components/ConclusionPanel.js
+++ react_web_page/src/components/ConclusionPanel.js
@@ -3,6 +3,22 @@ function ConclusionPanel()
     return (
         <div id="conclusion-content-container">
 
+            <h1 class="title" id="main-title">Пересекающиеся части в аудиозаписях музыкальных произведений</h1>
+
+            <div id="description-container">
+
+                <h2 class="title" id="description-title">Описание</h2>
+
+                <div id="description-text-container">
+
+                    <p>
+                        <span>Заключение</span>
+                    </p>
+
+                </div>
+
+            </div>
+
         </div>
     );
 }
